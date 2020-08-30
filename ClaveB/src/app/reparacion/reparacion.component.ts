cb6e0fa816e0ddecf4ef76304f1bb87fcf628e3e
Carrillo11 <renecarrillo120@gmail.com>
2020-08-30 17:16:46 -0600

    Se ha agregado diseño a la pagina

--- ClaveB/src/app/reparacion/reparacion.component.ts
+++ ClaveB/src/app/reparacion/reparacion.component.ts
@@ -10,39 +10,49 @@ import { ComponentFixtureAutoDetect } from '@angular/core/testing';
 })
 export class ReparacionComponent implements OnInit {
   registro=[];
-  cliente:any;
+  cliente:any
   nombre:string;
-  dui:string;
+  dui:number;
   vehiculo:string;
   costo:number;
   visita:number;
+  descuento:number;
+  total:number;
   contador:number;
-  constructor() { }
 
   ngOnInit() {
-  this.dui='';
+  this.dui;
   this.nombre='';
   this.vehiculo='';
-  this.costo=0;
-  this.visita=0;
+  this.costo;
+  this.descuento=0;
+  this.total=0;
   this.contador=0;
   }
 
   ingresar(){
+    let repdui:any =1;
+  repdui=this.registro.filter(element => element.dui == this.dui);
+  console.log(repdui.length++)
+  console.log(this.registro);
 
-    for(var i=0; i<=this.registro.length;i++){ 
-    if(this.registro[i].nombre==this.nombre){ //Parte donde no he podido resolver
-      this.visita++;
-    }
-    else{
-     this.visita=1;
-    }
-    console.log(this.registro[i]);
-}
+      if(repdui.length==1){
+        this.descuento=0;
+        this.total=this.costo
+      }
+     else if(repdui.length>=2 && repdui.length<=4){
+        this.descuento=this.costo*0.05;
+        this.total=this.costo-this.descuento;
+      }
+      else if(repdui.length>4){
+        this.descuento=this.costo*0.1;
+        this.total=this.costo-this.descuento;
+      }
+    
+    
 
-  this.cliente={"nombre":this.nombre,"dui":this.dui,"vehiculo":this.vehiculo,"costo":this.costo,"visita":this.visita,"contador":this.contador};
+  this.cliente={"nombre":this.nombre,"dui":this.dui,"vehiculo":this.vehiculo,"costo":this.costo,"descuento":this.descuento,"total":this.total,"visita":repdui.length,};
   this.registro.push(this.cliente);
   this.contador++;
- 
   }
   }
